cebbc0353faa289fade3cdc0fd175da3305e14bb
MuriloEduardo <muriloeduardoooooo@gmail.com>
2025-09-06 00:40:16 -0300

    Add events URL configuration and webhook view

--- events/views.py
+++ events/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def webhook(request):
+    print("Webhook received", request)
+    return HttpResponse("Hello world!")
