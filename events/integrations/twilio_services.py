5cf5cfa39daf2bcc85ef7d5e54c054e6b7df2f2a
MuriloEduardo <muriloeduardoooooo@gmail.com>
2025-09-05 23:16:33 -0300

    Add Twilio integration for sending WhatsApp messages and update requirements

---
+++ events/integrations/twilio_services.py
@@ -0,0 +1,23 @@
+import os
+
+from twilio.rest import Client
+
+
+class TwilioService:
+    def __init__(
+        self,
+        auth_token=os.getenv("TWILIO_AUTH_TOKEN"),
+        account_sid=os.getenv("TWILIO_ACCOUNT_SID"),
+        from_number=os.getenv("TWILIO_FROM_NUMBER"),
+    ):
+        self.from_number = from_number
+        self.client = Client(account_sid, auth_token)
+
+    def send(self, to_number, message):
+        message = self.client.messages.create(
+            body=message, from_=self.from_number, to=to_number
+        )
+
+        print("WhatsApp Message:", message)
+
+        return message
--- events/tasks.py
+++ events/tasks.py
@@ -1,6 +1,17 @@
 from celery import shared_task
 
+from events.integrations.twilio_services import TwilioService
+
+twilio_service = TwilioService()
+
 
 @shared_task(queue="ai-postprocessing")
 def postprocessing(*args, **kwargs):
     print("Postprocessing task executed with args:", args, "and kwargs:", kwargs)
+
+    result = twilio_service.send(
+        to_number="whatsapp:+5551974019092",
+        message="Postprocessing task completed successfully.",
+    )
+
+    print("WhatsApp Message sent:", result)
