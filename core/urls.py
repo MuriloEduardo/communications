26a7c09a619f3f5c1fd6db7883c440fb91cedd87
MuriloEduardo <muriloeduardoooooo@gmail.com>
2025-09-10 22:36:12 -0300

    Fix health check URL path by correcting leading slash in urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("/", views.health_check, name="health_check"),
     path("admin/", admin.site.urls),
     path("events/", include("events.urls")),
+    path("", views.health_check, name="health_check"),
 ]
